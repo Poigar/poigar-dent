e436973af9165b760a879c67ff4e4007933dd5b6
Reinards <hiperpile@gmail.com>
2018-06-10 12:15:28 +0300

    Js fix

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,14 @@
-function verify_deletion(){
-    if(!prompt('You are about to delete a user, continue?')){
-        alert('no');
+function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
     }
-}
+};
