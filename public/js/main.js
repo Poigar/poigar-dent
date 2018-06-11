21587802cace3eb2ee7c5b40c8cbe57b05de12aa
Reinards <hiperpile@gmail.com>
2018-06-11 11:51:23 +0300

    Added working hours in schedule

--- public/js/main.js
+++ public/js/main.js
@@ -44,21 +44,40 @@ function save_pattern(user_id){
     window.location = "edit_employee_pattern_action/"+user_id+"?pattern="+pattern;
 }
 
-function set_working_hours(day,){
+function set_working_hours(day, user_id){
+    console.log(user_id);
     day--;
     if(day<0) day=6;
 
-    var pattern = $(".hidden_pattern").html();
+    var pattern = "";
     var pattern_today = "";
+
+    if(user_id>=0){
+        pattern = $(".hidden_pattern_"+user_id).html(); 
+    }else{
+        pattern = $(".hidden_pattern").html();
+    }
+
+
 
     for(var i=day*24;i<(day*24 + 24);i++){
         pattern_today = pattern_today+pattern[i];
     }
 
-    for(var i=0;i<24;i++){
-        if(pattern_today[i] == "0"){
-            $('.js-time-row'+i).addClass('table-striped--gray');
+
+    if(user_id>0){
+        for(var i=0;i<24;i++){
+            if(pattern_today[i] == "0"){
+                $('.js-time-row'+i+'_'+user_id).addClass('table-striped--gray');
+            }
+        }
+    }else{
+        for(var i=0;i<24;i++){
+            if(pattern_today[i] == "0"){
+                $('.js-time-row'+i).addClass('table-striped--gray');
+            }
         }
     }
+
 
 }
